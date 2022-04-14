4026f2788fc114ee760073c1b5a7c95cddd89a5d
jmac222 <john.mcfarland222@gmail.com>
2022-04-13 22:51:54 -0700

    basically finsished code

--- src/App.js
+++ src/App.js
@@ -8,68 +8,77 @@ class App extends Component {
       search: "",
     };
   }
-  
-  async componentDidMount() {
-    const {search} = this.state
 
-    let url = `https://api.github.com/users${search}`
-    let result = null
+  async componentDidMount() {
+    const { search } = this.state;
+
+    let url = `https://api.github.com/users${search}`;
+    let result = null;
 
     try {
       result = await axios(url, {
         headers: {
-          Accept: 'application/json'
-        }
-      }) 
+          Accept: "application/json",
+        },
+      });
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
 
-    this.setState({user: result.data})
+    this.setState({ user: result.data });
   }
   async componentDidUpdate() {
-    const {search} = this.state
+    const { search } = this.state;
 
-    let url = `https://api.github.com/users${search}`
-    let result = null
+    let url = `https://api.github.com/users${search}`;
+    let result = null;
 
     try {
       result = await axios(url, {
         headers: {
-          Accept: 'application/json'
-        }
-      }) 
+          Accept: "application/json",
+        },
+      });
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
 
-    this.setState({user: result.data})
+    this.setState({ user: result.data });
   }
 
-
-
-
-
   render() {
-    console.log(this.state.user)
+    console.log(this.state.user);
 
-    return(
-      <div>
-        <input type='text' placeholder="Search github users..." onChange={(e) => {
-          this.setState({search: `/${e.target.value}`})
-          console.log(this.state.search)
-        }}/>
-        <div className="account" onClick={(e) => {
-          window.open(this.state.user.html_url)
-        }}>
-          <img src={this.state.user.avatar_url}></img>
-          <h1>{this.state.user.login}</h1>
-          <h2>{this.state.user.name}</h2>
-          <h3>{this.state.user.bio}</h3>
-          <p>{this.state.user.created_at}</p>
+    return (
+      <div className="app">
+        <div className="git">
+          <div className="form">
+            <h1>Search Github Users</h1>
+            <input
+              type="text"
+              placeholder="Search github users..."
+              onChange={(e) => {
+                this.setState({ search: `/${e.target.value}` });
+                console.log(this.state.search);
+              }}
+            />
+          </div>
+          <div
+            className="account"
+            onClick={(e) => {
+              window.open(this.state.user.html_url);
+            }}
+          >
+            <img src={this.state.user.avatar_url}></img>
+            <h1>{this.state.user.login}</h1>
+            <h2>{this.state.user.name}</h2>
+            <h3>{this.state.user.bio}</h3>
+            <p>Repositories: {this.state.user.public_repos}</p>
+            <p>{this.state.user.created_at}</p>
+          </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
