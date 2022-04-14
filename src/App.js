950e12d7ebc41b211d0be0153c5dccde5c0c0f6f
jmac222 <john.mcfarland222@gmail.com>
2022-04-13 21:36:58 -0700

    api works

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import axios from "axios";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: [],
+      search: "",
+    };
+  }
+  
+  async componentDidMount() {
+    const {search} = this.state
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let url = `https://api.github.com/users${search}`
+    let result = null
+
+    try {
+      result = await axios(url, {
+        headers: {
+          Accept: 'application/json'
+        }
+      }) 
+    } catch (error) {
+      console.log(error)
+    }
+
+    this.setState({user: result.data})
+  }
+  async componentDidUpdate() {
+    const {search} = this.state
+
+    let url = `https://api.github.com/users${search}`
+    let result = null
+
+    try {
+      result = await axios(url, {
+        headers: {
+          Accept: 'application/json'
+        }
+      }) 
+    } catch (error) {
+      console.log(error)
+    }
+
+    this.setState({user: result.data})
+  }
+
+
+
+
+
+  render() {
+    console.log(this.state.user)
+
+    return(
+      <div>
+        <input type='text' placeholder="Search github users..." onChange={(e) => {
+          this.setState({search: `/${e.target.value}`})
+          console.log(this.state.search)
+        }}/>
+        <div className="account" onClick={(e) => {
+          window.open(this.state.user.html_url)
+        }}>
+          <img src={this.state.user.avatar_url}></img>
+          <h1>{this.state.user.login}</h1>
+          <h2>{this.state.user.name}</h2>
+          <h3>{this.state.user.bio}</h3>
+          <p>{this.state.user.created_at}</p>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
